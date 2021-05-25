f1ee7c0f1abc0ec3be53823c46a5ca6c87147115
SolbiShin <sbshin.dev@gmail.com>
2021-05-25 22:35:40 +0900

    strapi 환경 구축

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '395afec541aa87798f31292ee33ef07f'),
+    },
+  },
+});
